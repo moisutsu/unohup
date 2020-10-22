c62e4ee4a86c06176ca64bef33a8225b4b9e46c0
moisutsu <moisutsu@gmail.com>
2020-10-23 00:03:35 +0900

    Change default value for log_file

--- src/opts.rs
+++ src/opts.rs
@@ -6,6 +6,6 @@ pub struct Opts {
     pub commands: Vec<String>,
     #[clap(short, long)]
     pub nice: Option<i32>,
-    #[clap(short, long)]
-    pub log_file: Option<String>,
+    #[clap(short, long, default_value = "/dev/null")]
+    pub log_file: String,
 }
