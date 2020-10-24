03c3e0c75dd555cd6839c0bd58ce1f00f2d162ce
moisutsu <moisutsu@gmail.com>
2020-10-25 01:38:02 +0900

    Change default value for nice

--- src/opts.rs
+++ src/opts.rs
@@ -1,11 +1,11 @@
-use clap::{Clap, crate_version};
+use clap::{crate_version, Clap};
 
 #[derive(Clap)]
 #[clap(version = crate_version!(), author = "moisutsu <moisutsu@gmail.com>")]
 pub struct Opts {
     pub commands: Vec<String>,
-    #[clap(short, long)]
-    pub nice: Option<i32>,
+    #[clap(short, long, default_value = "0")]
+    pub nice: i32,
     #[clap(short, long, default_value = "/dev/null")]
     pub log_file: String,
 }
